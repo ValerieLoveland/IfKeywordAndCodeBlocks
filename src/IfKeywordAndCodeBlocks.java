1c3992621a4175991c0d83caaa271655895081c0
valerieloveland <valerieloveland@gmail.com>
2019-01-14 21:32:29 -0500

    Initial commit

---
+++ src/IfKeywordAndCodeBlocks.java
@@ -0,0 +1,42 @@
+public class IfKeywordAndCodeBlocks {
+
+    public static void main(String[] args){
+boolean gameOver = true;
+int score= 800;
+int levelCompleted = 5;
+int bonus =100;
+
+
+
+/*if(score < 5000 && score >1000 ){
+    System.out.println("Your score was less than 5000 but greater than 1000");
+
+}else if (score <1000) {
+            System.out.println("Your score was less than 1000");
+
+}else{
+
+    System.out.println("Got here");
+}*/
+
+if(gameOver){
+    int finalScore = score + (levelCompleted*bonus);
+    finalScore += 1000;
+    System.out.println("Your final score was " + finalScore);
+    //deletes variables after it uses these? Weird.
+// ==true could also just be gameOver
+    //this is because of scope
+}
+
+if(gameOver){
+    score = 10000;
+    levelCompleted = 8;
+    bonus = 200;
+    int finalScore = score + (levelCompleted*bonus);
+    System.out.println("The second final score was " + finalScore);
+}
+
+
+    }
+
+}
